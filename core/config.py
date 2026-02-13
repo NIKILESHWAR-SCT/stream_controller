e4f56e8a553750a57df2514f3fbb1d3e954e80f3
Nikileshwar <Nikileshwar.V@solutionchamps.com>
2026-02-13 17:49:51 +0530

    Initial commit: streaming controller 13.02.2026

--- core/config.py
+++ core/config.py
@@ -1,19 +1,19 @@
 import os
-from typing import Literal
+from dotenv import load_dotenv
 
+load_dotenv(".env.development")  
 
 class Settings:
-    DB_NAME: str = os.getenv("DB_NAME", "streaming_controller")
-    DB_HOST: str = os.getenv("DB_HOST", "192.168.1.144")
-    DB_PORT: int = int(os.getenv("DB_PORT", "5432"))
-    DB_USER: str = os.getenv("DB_USER", "postgres")
-    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "vigilxdev")
-    # Async driver
-    DB_DRIVER: str = os.getenv("DB_DRIVER", "asyncpg")
-
-    SERVICE_DEVICE_ONBOARDING_URL: str = os.getenv(
-        "SERVICE_DEVICE_ONBOARDING_URL", "http://localhost:8001/api/v1/cameras/capabilities"
+    DATABASE_URL: str = os.getenv(
+        "DATABASE_URL",
+        "postgresql+asyncpg://postgres:postgres@localhost:5432/streaming_controller"
     )
 
+    SERVICE_DEVICE_ONBOARDING_URL: str = os.getenv(
+        "SERVICE_DEVICE_ONBOARDING_URL",
+        "http://localhost:8001/api/v1/cameras/capabilities"
+    )
+
+print("DATABASE_URL =", os.getenv("DATABASE_URL"))
 
 settings = Settings()
--- db/session.py
+++ db/session.py
@@ -1,19 +1,28 @@
-import asyncio
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from typing import AsyncGenerator
-from .base import Base
-from core.config import settings
 import logging
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+)
+
+from core.config import settings
+from .base import Base
 
 logger = logging.getLogger(__name__)
 
-
-DATABASE_URL = (
-    f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=False,
+    future=True,
 )
 
-engine = create_async_engine(DATABASE_URL, echo=False, future=True)
-async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+async_session = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
 
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
@@ -22,7 +31,6 @@ async def get_db() -> AsyncGenerator[AsyncSession, None]:
 
 
 async def init_db() -> None:
-    # Create tables if they don't exist
     logger.info("Initializing database and creating tables if needed")
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
